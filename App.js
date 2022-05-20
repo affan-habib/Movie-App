ce604a9ea7cd8900a38b4af7c2aa70059768a0f2
Affan-habib <affan.habib44@gmail.com>
2022-05-20 12:17:42 +0600

    redux setup and home page with mock data done

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Movies from './screens/Movies';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen options={{
+            tabBarIcon: () => (
+                <Icon name="movie" size={30} color="blue" />
+            ),
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "white",
+            },
+            headerStyle: {
+              backgroundColor: "#00669a",
+            },
+          }} name="Home" component={Movies} />
+        <Tab.Screen options={{
+            tabBarIcon: () => (
+                <Icon name="favorite" size={30} color="#900" />
+            ),
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "white",
+            },
+            headerStyle: {
+              backgroundColor: "#00669a",
+            },
+          }} name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
